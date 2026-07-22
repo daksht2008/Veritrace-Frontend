614866a106ebfa6832d22d09d67a9dbbabeb3eba
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-07-22 18:58:05 +0530

    fix: remove TracingBeam, restore normal scrollbar and full-width layout

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,7 +1,6 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import AmbientBackground from './AmbientBackground'
-import { TracingBeam } from './ui/tracing-beam'
 
 /** Shared layout boundary for every route. */
 export default function AppShell({ children }) {
@@ -9,9 +8,7 @@ export default function AppShell({ children }) {
     <>
       <AmbientBackground />
       <Navbar />
-      <TracingBeam className="px-0">
-        <main className="site-content min-h-[calc(100vh-200px)] pb-8">{children}</main>
-      </TracingBeam>
+      <main className="site-content min-h-[calc(100vh-200px)] pb-8">{children}</main>
       <Footer />
     </>
   )
