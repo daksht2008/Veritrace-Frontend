b5d8175c6efef076271293984c39492f8f5a273f
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-07-22 18:37:37 +0530

    style: remove custom cursor, update theme toggle to simple icon button

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,13 +1,11 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import AmbientBackground from './AmbientBackground'
-import CustomCursor from './CustomCursor'
 
 /** Shared layout boundary for every route. */
 export default function AppShell({ children }) {
   return (
     <>
-      <CustomCursor />
       <AmbientBackground />
       <Navbar />
       <main className="site-content min-h-[calc(100vh-200px)] pb-8">{children}</main>
