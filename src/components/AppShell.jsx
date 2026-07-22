bd979bfadb8bd24ddfdfff2fb491c17006bbe0c2
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-07-22 18:46:21 +0530

    feat: add TracingBeam scroll indicator to all pages via AppShell

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import AmbientBackground from './AmbientBackground'
+import { TracingBeam } from './ui/tracing-beam'
 
 /** Shared layout boundary for every route. */
 export default function AppShell({ children }) {
@@ -8,7 +9,9 @@ export default function AppShell({ children }) {
     <>
       <AmbientBackground />
       <Navbar />
-      <main className="site-content min-h-[calc(100vh-200px)] pb-8">{children}</main>
+      <TracingBeam className="px-0">
+        <main className="site-content min-h-[calc(100vh-200px)] pb-8">{children}</main>
+      </TracingBeam>
       <Footer />
     </>
   )
